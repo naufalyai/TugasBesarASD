5a22d57b0eb389b504c1069eaa1483f9293ee13f
wendianto <wendianto.96@gmail.com>
2015-11-21 17:30:35 +0700

    tubes.cpp

--- tubes.cpp
+++ tubes.cpp
@@ -34,3 +34,59 @@ void dealokasikomentar (address_k p)
 {
     free(p);
 }
+
+
+void insertFirstKomen(address &p,address_k c){
+    if (firstchild(p)!=NULL)
+    {
+        nextk(c)=firstchild(p);
+        prevk(firstchild(p))=c;
+        firstchild(p)=c;
+    }
+    else
+{
+        firstchild(p)=c;
+        lastchild(p)=c;
+}
+}
+
+void insertLastKomen(address &p,address_k c){
+  if (firstchild(p) != NULL)
+    {
+        prevk(c)=lastchild(p);
+        nextk(lastchild(p))=c;
+        lastchild(p)=c;
+    }
+    else
+    {
+        firstchild(p)=c;
+        lastchild(p)=c;
+    }
+}
+
+void insertAfterKomen(address &p,address_k c,string idkomenprec){
+    address_k z;
+    if(firstchild(p) != NULL)
+    {
+        z=firstchild(p);
+        while((infok(z).idkomentator)!=(idkomenprec) || (z!=lastchild(p))){
+            z=nextk(z);
+        }
+        if((infok(z).idkomentator)==idkomenprec && z!=lastchild(p)){
+            prevk(c)=z;
+            nextk(c)=nextk(z);
+            prevk(nextk(z))=c;
+            nextk(z)=c;}
+        else if (z==lastchild(p))
+        {
+            insertLastKomen(p,c);
+        }
+        else{
+            cout<<"Id komentar tidak ditemukan ";
+        }
+        }
+    else
+    {
+        insertFirstKomen(p,c);
+    }
+}
